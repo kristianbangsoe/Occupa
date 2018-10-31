51fbcbb5016ee4049b11a94dab4c32312407157d
Kristian Bangsø <kette_bangsoe@live.dk>
2018-10-31 11:30:06 +0100

    Started project

---
+++ Occupa/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Occupa.Models;
+
+namespace Occupa.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}

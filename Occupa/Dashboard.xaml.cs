e79eb45bf6ba5eab052cd2aaca5ff4079605e077
Kristian Bangsø <kette_bangsoe@live.dk>
2018-11-01 08:45:47 +0100

    Dashboard – Navigation

---
+++ Occupa/Dashboard.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Occupa
+{
+    public partial class Dashboard : ContentPage
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}

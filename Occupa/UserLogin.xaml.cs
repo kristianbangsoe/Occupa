8e3d40e3ab23affa9df207132a6cf29f31348649
Stefan Turcanu <stefanturcanu@live.com>
2018-12-18 15:24:21 +0100

    UserLogin view
    Still requires vertical centring

---
+++ Occupa/UserLogin.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Occupa
+{
+    public partial class UserLogin : ContentPage
+    {
+        public UserLogin()
+        {
+            InitializeComponent();
+        }
+    }
+}

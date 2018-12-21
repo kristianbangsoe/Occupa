c2d41894d34ed7fffafe322246237c6f979aee99
Stefan Turcanu <stefanturcanu@live.com>
2018-12-21 10:55:42 +0100

    Included PassReset views to new project

---
+++ occupaTab/Views/UserForgotPass.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using occupaTab.Views;
+
+namespace occupaTab.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UserForgotPass : ContentPage
+    {
+        public UserForgotPass()
+        {
+            InitializeComponent();
+        }
+
+        public async void HandleLogin(object sender, EventArgs e)
+        {
+            var MainPage = new UserLogin();
+            await Navigation.PushModalAsync(MainPage);
+        }
+
+        public async void HandleResetPass(object sender, EventArgs e)
+        {
+            var MainPage = new UserResetPass();
+            await Navigation.PushModalAsync(MainPage);
+        }
+
+    }
+}
---
+++ occupaTab/Views/UserResetPass.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using occupaTab.Views;
+
+namespace occupaTab.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UserResetPass : ContentPage
+    {
+        public UserResetPass()
+        {
+            InitializeComponent();
+        }
+
+        public async void HandleLogin(object sender, EventArgs e)
+        {
+            var MainPage = new UserLogin();
+            await Navigation.PushModalAsync(MainPage);
+        }
+
+    }
+}

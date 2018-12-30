5ce15da6707fa8449e545e23e21e12a16eeb2280
SørenHøjby <soer792e@edu.eal.dk>
2018-12-30 22:55:00 +0100

    added picture for video thumb, added the pages for muskler ex1 and 2 and finished the muskler page

--- occupaTab/Views/Exercise_Anatomi_Muskler.xaml.cs
+++ occupaTab/Views/Exercise_Anatomi_Muskler.xaml.cs
@@ -17,17 +17,31 @@ namespace occupaTab.Views
 			InitializeComponent ();
 		}
         
-        private async void clicked_btn_anatomi_muskler(object sender, EventArgs e)
+        private async void Clicked_btn_anatomi_muskler(object sender, EventArgs e)
         {
             var Exercise_Anatomi_Muskler = new Exercise_Anatomi_Muskler();
             await Navigation.PushAsync(Exercise_Anatomi_Muskler);
         }
 
-        private async void clicked_btn_anatomi_skelettet(object sender, EventArgs e)
+        private async void Clicked_btn_anatomi_skelettet(object sender, EventArgs e)
         {
             var Exercise_Anatomi_Skelettet = new Exercise_Anatomi_Skelettet();
             await Navigation.PushAsync(Exercise_Anatomi_Skelettet);
         }
 
+   
+        private async void Clicked_btn_anatomi_muskler_ex1(object sender, EventArgs e)
+        {
+
+            var Exercise_Anatomi_Muskler_ex1 = new Exercise_Anatomi_Muskler_ex1();
+            await Navigation.PushAsync(Exercise_Anatomi_Muskler_ex1);
+
+        }
+        private async void Clicked_btn_anatomi_muskler_ex2(object sender, EventArgs e)
+        {
+            var Exercise_Anatomi_Muskler_ex2 = new Exercise_Anatomi_Muskler_ex2();
+            await Navigation.PushAsync(Exercise_Anatomi_Muskler_ex2);
+
+        }
     }
 }
---
+++ occupaTab/Views/Exercise_Anatomi_Muskler_ex1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace occupaTab.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Exercise_Anatomi_Muskler_ex1 : ContentPage
+	{
+		public Exercise_Anatomi_Muskler_ex1 ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
